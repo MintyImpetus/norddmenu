fc55ff182db7b07e96fb1ec5a68f39b70ae9e3ee
MintyImpetus <tooster742@gmail.com>
2022-04-12 04:14:19 +0930

    Added Nord Dmenu

---
+++ config.h
@@ -0,0 +1,31 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"monospace:size=10",
+	"JoyPixels:pixelsize=10:antialias=true:autohint=true"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#d8dee9", "#434c5e" },
+	[SchemeSel] = { "#5e81ac", "#4c566a" },
+	[SchemeOut] = { "#00ffff", "#00ffff" },
+        [SchemeNormHighlight] = { "#d8dee9", "#b48ead" },
+        [SchemeSelHighlight] = { "#d8dee9", "#bf616a" },
+        [SchemeOutHighlight] = { "#ffc978", "#00ffff" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
+
+static unsigned int border_width = 6;
